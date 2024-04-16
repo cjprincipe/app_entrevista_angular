b4605a1134350f981767fa158b274a22eebd43b7
Clark Julian Principe Ayala <cprincip@emeal.nttdata.com>
2024-04-16 00:46:31 -0500

    Test units para LoginComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,11 +19,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('app_entrevista_angular');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app_entrevista_angular app is running!');
-  });
 });
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,21 +1,117 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { NO_ERRORS_SCHEMA, inject } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Store, StoreModule } from '@ngrx/store';
+import { User } from 'src/app/models/user.model';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  let componentFixture: ComponentFixture<LoginComponent>;
+
+  let voidFake = () => {};
+
+  let storeUserMock: Store<User>;
+  let routerMock: Router;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      declarations: [LoginComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [
+        FormsModule,
+        CommonModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot({}),
+        AppRoutingModule
+      ],
+      providers: [ 
+        {
+          provide: Router,
+          useValue: {
+            navigate: jasmine.createSpy('navigate')
+          }
+        },
+        {
+          provide: Store,
+          useValue: { 
+            dispatch: jasmine.createSpy('dispatch').and.callFake(voidFake)
+          }
+        }
+      ]
+    }).compileComponents();
+
+    componentFixture = TestBed.createComponent(LoginComponent);
+    component = componentFixture.componentInstance;
+
+    routerMock = TestBed.inject(Router);
+    storeUserMock = TestBed.inject(Store);
+
+    component.formLogin = new FormGroup({
+      user: new FormControl(),
+      pwd: new FormControl()
+    })
+    
   });
 
-  it('should create', () => {
+  it('Init compomente', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Login KO mensaje error', () => {
+
+    component.formLogin = new FormGroup({
+      user: new FormControl('test00009'),
+      pwd: new FormControl('test000010')
+    })
+
+    component.login();
+
+    expect(component.messageErrorLogin).toBe('Credenciales incorrectas!');
+  });
+
+  
+  it('Login OK', () => {
+    spyOn(component, 'saveInStoreUser').and.callFake(voidFake);
+    
+    component.formLogin = new FormGroup({
+      user: new FormControl('test01'),
+      pwd: new FormControl('test01')
+    })
+
+    component.login();
+
+    expect(component.saveInStoreUser).toHaveBeenCalled();
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/dashboard']);
+  });
+
+  it('Almacenar en memoria ngrx reducer', () => {
+    component.formLogin = new FormGroup({
+      user: new FormControl('test01'),
+      pwd: new FormControl('test01')
+    })
+
+    component.saveInStoreUser();
+
+    expect(storeUserMock.dispatch).toHaveBeenCalled();
+  })
+
+  it('Validar credenciales', () => {
+    component.formLogin = new FormGroup({
+      user: new FormControl('test01'),
+      pwd: new FormControl('test01')
+    })
+
+    const result = component.validateCredential(component.formLogin);
+
+    expect(result).toBeTruthy();
+
+  })
+  
+
 });
